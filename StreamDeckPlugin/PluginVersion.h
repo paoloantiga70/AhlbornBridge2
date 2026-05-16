2b43099d38bc469e4eef655138ac986503bbc84e
paoloantiga70 <paoloantiga@gmail.com>
2026-05-16 18:57:10 +0200

    Commit v 0.0.38

--- StreamDeckPlugin/PluginVersion.h
+++ StreamDeckPlugin/PluginVersion.h
@@ -8,8 +8,8 @@
 // ---------------------------------------------------------------
 #define PLUGIN_VERSION_MAJOR 1
 #define PLUGIN_VERSION_MINOR 0
-#define PLUGIN_VERSION_PATCH 38
-#define PLUGIN_VERSION "1.0.38"
+#define PLUGIN_VERSION_PATCH 39
+#define PLUGIN_VERSION "1.0.39"
 
 // GitHub repository for auto-update checks.
 // Change these to match your GitHub repository.
--- Test1/Version.h
+++ Test1/Version.h
@@ -8,8 +8,8 @@
 // ---------------------------------------------------------------
 #define APP_VERSION_MAJOR 0
 #define APP_VERSION_MINOR 3
-#define APP_VERSION_PATCH 38
-#define APP_VERSION "0.3.38"
+#define APP_VERSION_PATCH 39
+#define APP_VERSION "0.3.39"
 
 // GitHub repository for auto-update checks.
 // Change these to match your GitHub repository.
